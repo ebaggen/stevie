28fbddc1360ab8b10fdd77700b7d26c5f0244e14
ebaggen <eric.baggen@gmail.com>
2016-08-04 14:28:35 -0400

    Create stevie.h

---
+++ firmware/stevie.h
@@ -0,0 +1,13 @@
+#include "InternetButton/InternetButton.h"
+
+class StevieButton : public InternetButton {
+	public:
+		const uint8_t top = 4;
+		const uint8_t left = 5;
+		const uint8_t bottom = 6;
+		const uint8_t right = 7;
+		
+		uint8_t countButtonsOn(void);
+		uint8_t anyButtonOn(void);
+		uint8_t buttonOn(uint8_t i);
+};
